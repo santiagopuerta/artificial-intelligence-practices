88921d085a6e9e0134fbe9984361b4edf9c0aece
elwarxy <elwarxy@gmail.com>
2013-04-02 12:28:11 +0200

    Practica 1 - IA/nVersion 1. Nivel de suciedad: 80068

---
+++ agent.cpp
@@ -0,0 +1,151 @@
+/*
+ * Santiago J. Puerta Correa
+ */
+#include "agent.h"
+#include "environment.h"
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <vector>
+#include <utility>
+
+
+using namespace std;
+
+// -----------------------------------------------------------
+Agent::ActionType Agent::Think()
+{
+	ActionType accion;
+	if(bump_){
+		switch(orientacion){
+			case 0:
+				mapa[(x+9)%10][y] = -1;
+				break;
+			case 1:
+				mapa[x][(y+1)%10] = -1;
+				break;
+			case 2:
+				mapa[(x+1)%10][y] = -1;
+				break;
+			case 3:
+				mapa[x][(y+9)%10] = -1;
+				break;
+		}
+		//cout << "-- Pared --" << endl;
+	}else if(ultima == actFORWARD){
+		CambiaPos();
+		mapa[x][y] += 1;
+	}
+	if(dirty_){
+		accion = actSUCK;
+	}else{
+		accion = DondeGirar();
+	}
+	//PintaMatriz();
+	//cout << "Posicion actual x: " << x << " y: " << y << endl;
+	ultima = accion;
+	return accion;
+}
+
+Agent::ActionType Agent::DondeGirar(){
+	ActionType accion;
+	int delante, detras, izquierda, derecha, valor;
+	//cout << "Orientacion: " << orientacion << endl;
+	switch(orientacion){
+		case 0:
+			delante = mapa[(x+9)%10][y];
+			detras = mapa[(x+1)%10][y];
+			izquierda = mapa[x][(y+9)%10];
+			derecha = mapa[x][(y+1)%10];
+			break;
+		case 1:
+			delante = mapa[x][(y+1)%10];
+			detras = mapa[x][(y+9)%10];
+			izquierda = mapa[(x+9)%10][y];
+			derecha = mapa[(x+1)%10][y];
+			break;
+		case 2:
+			delante = mapa[(x+1)%10][y];
+			detras = mapa[(x+9)%10][y];
+			izquierda = mapa[x][(y+1)%10];
+			derecha = mapa[x][(y+9)%10];
+			break;
+		case 3:
+			delante = mapa[x][(y+9)%10];
+			detras = mapa[x][(y+1)%10];
+			izquierda = mapa[(x+1)%10][y];
+			derecha = mapa[(x+9)%10][y];
+			break;
+	}
+	//cout << "delante: " << delante << endl;
+	accion = actFORWARD;
+	valor = delante;
+	if(derecha != -1 && derecha < valor){
+		accion = actTURN_R;
+		orientacion = (orientacion + 1)%4;
+		valor = derecha;
+	}
+	if(izquierda != -1 && izquierda < valor){
+		accion = actTURN_L;
+		orientacion = (orientacion + 3)%4;
+		valor = izquierda;
+	}
+	if(valor == -1){
+		accion = actTURN_R;
+		orientacion = (orientacion + 1)%4;
+	}
+	//cout << "Orientacion despues de accion: " << orientacion << endl;
+	return accion;
+}
+
+
+//Para pintar la matriz mapa.
+void Agent::PintaMatriz(){
+    for(int f=0; f<10; f++){
+        for(int c=0; c<10; c++){
+            cout << setw(2) << mapa[f][c] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void Agent::CambiaPos(){
+	// Si no choca
+	switch(orientacion){
+		case 0:
+			x=(x+9)%10;
+			break;
+		case 1:
+			y=(y+1)%10;
+			break;
+		case 2:
+			x=(x+1)%10;
+			break;
+		case 3:
+			y=(y+9)%10;
+			break;
+	}
+}
+
+
+
+// -----------------------------------------------------------
+void Agent::Perceive(const Environment &env)
+{
+	bump_ = env.isJustBump();
+	dirty_ = env.isCurrentPosDirty();
+}
+// -----------------------------------------------------------
+string ActionStr(Agent::ActionType accion)
+{
+    switch (accion)
+	{
+	case Agent::actFORWARD: return "FORWARD";
+	case Agent::actTURN_L: return "TURN LEFT";
+	case Agent::actTURN_R: return "TURN RIGHT";
+	case Agent::actSUCK: return "SUCK";
+	case Agent::actIDLE: return "IDLE";
+	default: return "???";
+	}
+}
---
+++ agent.h
@@ -0,0 +1,62 @@
+/*
+ * Santiago J. Puerta Correa
+ */
+#ifndef AGENT__
+#define AGENT__
+
+#include <string>
+using namespace std;
+
+// -----------------------------------------------------------
+//				class Agent
+// -----------------------------------------------------------
+class Environment;
+class Agent
+{
+public:
+	Agent():bump_(false), dirty_(false){
+	    x = 0;
+	    y = 0;
+	    orientacion = 0;
+	    vuelta = false;
+	    giro = false;
+	    choco = false;
+	    cuenta = 0;
+	    ultima = actFORWARD;
+        for(int f=0; f<10; f++){
+            for(int c=0; c<10; c++){
+                        mapa[f][c] = 0;
+            }
+        }
+	}
+	enum ActionType
+	{
+	    actFORWARD,
+	    actTURN_L,
+	    actTURN_R,
+	    actSUCK,
+	    actIDLE
+	};
+	
+
+	void Perceive(const Environment &env);
+	ActionType Think();
+	void PintaMatriz();
+	void CambiaPos();
+	ActionType DondeGirar();
+private:
+	bool bump_, dirty_;
+    int mapa[10][10];
+    int x,y; // Posiciones actuales.
+    int orientacion;
+    bool vuelta;
+    ActionType ultima;
+    bool giro;
+    bool choco;
+    int cuenta;
+
+};
+
+string ActionStr(Agent::ActionType);
+
+#endif
